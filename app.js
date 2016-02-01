c036ed036b4dc7f89b640d32ad4e0e8ca4f62153
Thibaut Géry <thibaut.gery@gmail.com>
2016-02-01 17:22:33 +0100

    add app

---
+++ app.js
@@ -0,0 +1,18 @@
+
+var express = require('express');
+var mongoose = require('mongoose');
+
+var uri = 'mongodb://myDb:27017/mongoose-shared-connection';
+global.db = mongoose.createConnection(uri);
+
+var routes = require('./routes');
+
+var app = express();
+app.get('/', routes.home);
+app.get('/insert', routes.insert);
+app.get('/name', routes.modelName);
+
+app.listen(3004, function() {
+  console.log('Server listening on port 3004');
+  console.log('Sever successfully started.');
+});
